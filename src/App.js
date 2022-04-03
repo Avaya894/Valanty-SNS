813d98aff0ea8e1334fab55fb6a959edc99baf39
34-anish <luckyanish47@gmail.com>
2022-04-03 21:52:33 +0545

    Context Api remaining

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Pages/home/Home";
-import Profile from "./Pages/profile/Profile";
-import Login from "./Pages/login/Login";
-import Register from "./Pages/register/Register";
-function App() {
-  return <Profile />;
-}
-
-export default App;
